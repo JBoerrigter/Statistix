175b2d0f3a4eef99368e4bef5bd6f0616ad8f23f
Jascha Boerrigter <3X4r3mE@live.de>
2021-01-19 18:57:51 +0100

    Fixed naming error

--- src/main/java/de/jaschaboerrigter/statistix/utils/config/ModConfig.java
+++ src/main/java/de/jaschaboerrigter/statistix/utils/config/ModConfig.java
@@ -3,6 +3,6 @@ package de.jaschaboerrigter.statistix.utils.config;
 public final class ModConfig {
     
     public static final String MOD_ID = "statistix";
-    public static final String CREATIVE_TAB = "Statistix";
+    public static final String CREATIVE_TAB = "statistix";
 
 }
